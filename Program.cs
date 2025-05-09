c5e27d37da6d36213a4ad9764be49c6b95ae9c9d
Taylo <Taylo@MARIEL>
2025-05-08 22:40:10 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System;
+
+class Program
+{
+    static int ClimbRecursive(int n)
+    {
+        if (n <= 2) return n;
+        return ClimbRecursive(n - 1) + ClimbRecursive(n - 2);
+    }
+
+
+    static void Main()
+    {
+        Console.WriteLine("Input: n = ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        int result = ClimbRecursive(n);
+        Console.WriteLine($"Output: {result}");
+
+    }
+}
